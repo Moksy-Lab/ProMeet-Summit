b607f62f30944f82b74e78683f2564bf8937fc09
mr-tajul-Islam <tajul.azazil@gmail.com>
2025-04-21 10:47:38 +0600

    ok

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,39 @@
+{
+           "@context": "http://www.schema.org",
+           "@type": "ProfessionalService",
+           "name": "ProMeet",
+           "title": "ProMeet - Online Learning for Personal Growth",
+           "url": "/",
+           "sameAs": [
+              ""
+           ],
+           "logo": "https://moksy.com/api/build/uploads/12575403/uploads/logo.png",
+           "priceRange": "$$$",
+           "image": "https://moksy.com/api/build/uploads/12575403/uploads/socialshare.png",
+           "description": "ProMeet offers online learning for personal growth. Discover our courses and start your journey towards self-improvement today.",
+           "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "3-5, Marco Polo House, Lansdowne Road",
+              "addressLocality": "London",
+              "addressRegion": "Surrey",
+              "postalCode": "CR0 2BX",
+              "addressCountry": "United Kingdom"
+           },
+           "email": "support@moksy.com",
+           "geo": {
+              "@type": "GeoCoordinates",
+              "latitude": "51.37683",
+              "longitude": "-0.09728"
+           },
+           
+           "openingHours": "Mo 09:00-17:00 Tu 09:00-17:00 We 09:00-17:00 Th 09:00-17:00 Fr 09:00-17:00",
+           "telephone": "+44 0808 175 1749",
+           "aggregateRating": {
+                 "@type": "AggregateRating",
+                 "ratingValue": "",
+                 "ratingCount": ""
+               },
+          "datePublished": ""
+         }
+document.write(new Date().getFullYear());
+Value for custom_js_code
